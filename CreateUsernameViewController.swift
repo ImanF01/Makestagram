9b284731b6dd48e773e769934189a61ac7682b5c
ImanF01 <imanfahmy01@gmail.com>
2017-06-26 13:39:21 -0700

    created login and asked user for info

---
+++ CreateUsernameViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CreateUsernameViewController.swift
+//  Makestagram
+//
+//  Created by Iman F on 6/26/17.
+//  Copyright © 2017 Iman F (group project). All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+class CreateUsernameViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nextButton.layer.cornerRadius = 6
+    }
+    
+    @IBAction func nextButtonTapped(_ sender: Any) {
+        guard let firUser = Auth.auth().currentUser,
+            let username = usernameTextField.text,
+            !username.isEmpty else { return }
+        UserService.create(firUser, username: username) { (user) in
+            guard let user = user else {
+                return
+            }
+            User.setCurrent(user)
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: .main)
+            if let initialViewController = storyboard.instantiateInitialViewController() {
+                self.view.window?.rootViewController = initialViewController
+                self.view.window?.makeKeyAndVisible()
+            }
+        }
+        let userAttrs = ["username": username]
+        let ref = Database.database().reference().child("users").child(firUser.uid)
+        ref.setValue(userAttrs) { (error, ref) in
+            if let error = error {
+                assertionFailure(error.localizedDescription)
+                return
+            }
+            ref.observeSingleEvent(of: .value, with: { (snapshot) in
+                _ = User(snapshot: snapshot)
+            })
+        }
+    }
+}
--- LoginViewController.swift
+++ LoginViewController.swift
@@ -7,9 +7,21 @@
 //
 
 import UIKit
-
+import FirebaseAuth
+import FirebaseAuthUI
+import FirebaseDatabase
+typealias FIRUser = FirebaseAuth.User
 class LoginViewController: UIViewController {
-
+    let user: FIRUser? = Auth.auth().currentUser
+    @IBOutlet weak var loginButton: UIButton!
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else{ return }
+        authUI.delegate = self
+        let authViewController = authUI.authViewController()
+        present(authViewController,animated: true)
+        print("login button tapped")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,11 +32,8 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
@@ -32,4 +41,34 @@ class LoginViewController: UIViewController {
     }
     */
 
+}
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+        }
+        guard let user = user
+            else { return }
+        let userRef = Database.database().reference().child("users").child(user.uid)
+        userRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            if let user = User(snapshot: snapshot) {
+                print("Welcome back, \(user.username).")
+            } else {
+                print("New user!")
+            }
+        })
+        userRef.observeSingleEvent(of: .value, with: { [unowned self] (snapshot) in
+        if let user = User(snapshot: snapshot) {
+            User.setCurrent(user)
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: .main)
+            if let intialViewController = storyboard.instantiateInitialViewController() {
+                self.view.window?.rootViewController = intialViewController
+                self.view.window?.makeKeyAndVisible()
+            }
+        } else {
+            self.performSegue(withIdentifier: "toCreateUsername", sender: self)
+            }
+        })
+    }
 }
